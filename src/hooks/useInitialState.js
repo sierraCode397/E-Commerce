5da5723bedd05afdb30a4b63503abd47c6379450
izaack107 <izaack107@gmail.com>
2023-03-01 14:04:18 -0600

    Se agrego la funcion para eliminar productos del carrito

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -14,9 +14,18 @@ const useInitialState = () => {
 		});
 	};
 
+	const removeFromCart = (payload) => {
+		setState({
+			...state,
+			cart: state.cart.filter(items => items.id != payload.id),
+		})
+	}
+
 	return {
 		state,
-		addToCart
+		addToCart,
+		removeFromCart,
+
 	}
 }
 
